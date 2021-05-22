60caa5150c1b6655c09b5f803c4b30fb0cb99a0f
Heroyt <vojik@wboy.cz>
2021-05-22 18:08:44 +0200

    :fire: Unused sorter methods
    Signed-off-by: Heroyt <vojik@wboy.cz>

--- src/TournamentGenerator/Helpers/Sorter/TeamSorter.php
+++ src/TournamentGenerator/Helpers/Sorter/TeamSorter.php
@@ -44,70 +44,12 @@ class TeamSorter implements BaseSorter
 	}
 
 	/**
-	 * Sort teams in group by defined ordering type
-	 *
-	 * @param Team[]                      $teams    Array of teams to be sorted
-	 * @param Group                       $group    Group to get the results from
-	 * @param string                      $ordering What to order by (\TournamentGenerator\Constants::POINTS / \TournamentGenerator\Constants::SCORE)
-	 *
-	 * @return Team[] Sorted array of teams
-	 * @throws Exception
-	 */
-	public static function sortGroup(array &$teams, Group $group, string $ordering = Constants::POINTS) : array {
-		if (!in_array($ordering, Constants::OrderingTypes, true)) {
-			throw new Exception('Unknown ordering type `'.$ordering.'`');
-		}
-
-		self::$ids = [$group->getId()];
-
-		switch ($ordering) {
-			case Constants::POINTS:
-				usort($teams, [__CLASS__, 'sortTeamsByPoints']);
-				break;
-			case Constants::SCORE:
-				usort($teams, [__CLASS__, 'sortTeamsByScore']);
-				break;
-		}
-
-		return $teams;
-	}
-
-	/**
-	 * Sort teams in round by defined ordering type
-	 *
-	 * @param Team[]                      $teams    Array of teams to be sorted
-	 * @param Round                       $round    Round to get the results from
-	 * @param string                      $ordering What to order by (\TournamentGenerator\Constants::POINTS / \TournamentGenerator\Constants::SCORE)
-	 *
-	 * @return Team[] Sorted array of teams
-	 * @throws Exception
-	 */
-	public static function sortRound(array &$teams, Round $round, string $ordering = Constants::POINTS) : array {
-		if (!in_array($ordering, Constants::OrderingTypes, true)) {
-			throw new Exception('Unknown ordering type `'.$ordering.'`');
-		}
-
-		self::$ids = $round->getGroupsIds();
-
-		switch ($ordering) {
-			case Constants::POINTS:
-				usort($teams, [__CLASS__, 'sortTeamsByPoints']);
-				break;
-			case Constants::SCORE:
-				usort($teams, [__CLASS__, 'sortTeamsByScore']);
-				break;
-		}
-
-		return $teams;
-	}
-
-	/**
 	 * Sorter function for usort by points
 	 *
 	 * @param Team $a First team
 	 * @param Team $b Second team
 	 */
-	private static function sortTeamsByPoints(Team $a, Team $b) : int {
+	protected static function sortTeamsByPoints(Team $a, Team $b) : int {
 		$groupsIds = self::$ids;
 		if ($a->sumPoints($groupsIds) === $b->sumPoints($groupsIds) && $a->sumScore($groupsIds) === $b->sumScore($groupsIds)) {
 			return 0;
@@ -124,7 +66,7 @@ class TeamSorter implements BaseSorter
 	 * @param Team $a First team
 	 * @param Team $b Second team
 	 */
-	private static function sortTeamsByScore(Team $a, Team $b) : int {
+	protected static function sortTeamsByScore(Team $a, Team $b) : int {
 		$groupsIds = self::$ids;
 		if ($a->sumScore($groupsIds) === $b->sumScore($groupsIds)) {
 			return 0;
