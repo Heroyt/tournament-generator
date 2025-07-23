64eb154b7a8a302162846e4905b6af7cde65ee26
Tomáš Vojík <vojik@wboy.cz>
2025-07-23 14:00:45 +0200

    :heavy_plus_sign: Added new code quality tools
    Signed-off-by: Tomáš Vojík <vojik@wboy.cz>

---
+++ rector.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+                   ->withPaths(
+                       [
+                           __DIR__.'/src',
+                           __DIR__.'/tests',
+                       ]
+                   )
+                   ->withPhpSets(php84: true)
+                   ->withAttributesSets(
+                       phpunit: true,
+                   )
+                   ->withPreparedSets(
+                       deadCode        : true,
+                       codeQuality     : true,
+                       typeDeclarations: true,
+                       privatization   : true,
+                       naming          : true,
+                   )
+                   ->withComposerBased(
+                       phpunit   : true,
+                       netteUtils: true,
+                   );
